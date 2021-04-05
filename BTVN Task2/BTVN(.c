4d952914bcb275fb341e26fe85b55d37eaa23474
army220pro <81789631+army220pro@users.noreply.github.com>
2021-04-05 08:28:59 +0700

    BTVN TASK2

---
+++ BTVN Task2/BTVN(.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() 
+{
+	char abr; /* abc of type character*/
+	int xyz; /* xyz of type integer*/
+	float length; /* length of type float*/
+	double area; /* area of type double*/
+	long litteyrs; /* liteyrs of type long int */
+	short arm; /* arm of type short integer*/
+	return 0;
+}
