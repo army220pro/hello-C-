83becea0c8309590a1f004345c1199557d4b3bcf
army220pro <81789631+army220pro@users.noreply.github.com>
2021-04-07 19:19:37 +0700

    Create BTVN_tinh diem trung binh 3 mon toan ly hoa).c

---
+++ btvn taks1/BTVN_tinh diem trung binh 3 mon toan ly hoa).c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) 
+{
+	int a;/* diem mon toan */
+	int b;/* diem mon ly */
+	int c;/* diem mon hoa */
+	int Tong ; /* tong diem ba mon */
+	int z;/* diem trung binh mon */
+	
+	printf("nhap diem mon toan \n");
+	scanf("%d", &a);
+	
+	printf("nhap diem mon ly \n");
+	scanf("%d", &b);
+	
+	printf("nhap diem mon hoa \n");
+	scanf("%d", &c);
+	
+	Tong=a+b+c;
+	printf("Tong diem 3 mon la:%d \n",Tong);
+	
+	z=Tong/3;
+	printf("diem trung binh mon:%d",z);
+	return 0;
+}
